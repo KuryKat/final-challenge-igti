ed7580807af2e5aa203c706c1e6bf851c705590f
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-11-14 01:25:00 -0300

    Remove Exaggerated Comments
     (They were structural aid)

--- backend/index.js
+++ backend/index.js
@@ -1,72 +1,52 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const routes = require('./routes/routes');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const routes = require('./routes/routes')
+const path = require('path')
+const dotenv = require('dotenv')
 
-/**
- * Faz a leitura do arquivo
- * ".env" por padrão
- */
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-/**
- * Vinculando o React ao app
- */
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-/**
- * Rota raiz
- */
 app.get('/api/', (_, response) => {
-  response.send({
-    message:
-      'Bem-vindo à API de lançamentos. Acesse /transaction e siga as orientações',
-  });
-});
+    response.send({
+        message:
+            'Bem-vindo à API de lançamentos. Acesse /transaction e siga as orientações',
+    })
+})
 
-/**
- * Rotas principais do app
- */
-app.use('/api/transaction', routes);
+app.use('/api/transaction', routes)
 
-/**
- * Conexão ao Banco de Dados
- */
-const { DB_CONNECTION } = process.env;
+const { DB_CONNECTION } = process.env
+console.log('Iniciando conexão ao MongoDB...')
 
-console.log('Iniciando conexão ao MongoDB...');
 mongoose.connect(
-  DB_CONNECTION,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      connectedToMongoDB = false;
-      console.error(`Erro na conexão ao MongoDB - ${err}`);
+    DB_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    err => {
+        if (err) {
+            connectedToMongoDB = false
+            console.error(`Erro na conexão ao MongoDB - ${err}`)
+        }
     }
-  }
-);
+)
 
-const { connection } = mongoose;
+const { connection } = mongoose
 
 connection.once('open', () => {
-  connectedToMongoDB = true;
-  console.log('Conectado ao MongoDB');
+    connectedToMongoDB = true
+    console.log('Conectado ao MongoDB')
 
-  /**
-   * Definição de porta e
-   * inicialização do app
-   */
-  const APP_PORT = process.env.PORT || 3001;
-  app.listen(APP_PORT, () => {
-    console.log(`Servidor iniciado na porta ${APP_PORT}`);
-  });
-});
+    const APP_PORT = process.env.PORT || 3001
+    app.listen(APP_PORT, () => {
+        console.log(`Servidor iniciado na porta ${APP_PORT}`)
+    })
+})
