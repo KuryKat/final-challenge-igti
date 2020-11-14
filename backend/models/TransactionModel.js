601b89c77470d352bec7002073d12e33c70b3531
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-11-14 11:49:38 -0300

    Update Transaction Database Model

--- backend/models/TransactionModel.js
+++ backend/models/TransactionModel.js
@@ -1,51 +1,50 @@
-import { Schema, model } from 'mongoose'
+import mongoose from 'mongoose'
+const { Schema, model } = mongoose
 
 export const TransactionModel = model(
     'transaction',
-    Schema(
-        {
-            description: {
-                type: String,
-                required: true,
-            },
-            value: {
-                type: Number,
-                required: true,
-            },
-            category: {
-                type: String,
-                required: true,
-            },
-            year: {
-                type: Number,
-                required: true,
-            },
-            month: {
-                type: Number,
-                required: true,
-            },
-            day: {
-                type: Number,
-                required: true,
-            },
-            yearMonth: {
-                type: String,
-                required: true,
-            },
-            yearMonthDay: {
-                type: String,
-                required: true,
-            },
-            type: {
-                type: String,
-                required: true,
-            },
-            lastModified: {
-                type: Date,
-                required: true,
-                default: new Date(),
-            },
+    Schema({
+        description: {
+            type: String,
+            required: true,
         },
-        'transactions'
-    )
+        value: {
+            type: Number,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        year: {
+            type: Number,
+            required: true,
+        },
+        month: {
+            type: Number,
+            required: true,
+        },
+        day: {
+            type: Number,
+            required: true,
+        },
+        yearMonth: {
+            type: String,
+            required: true,
+        },
+        yearMonthDay: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        lastModified: {
+            type: Date,
+            required: true,
+            default: new Date(),
+        },
+    }),
+    'transactions'
 )
