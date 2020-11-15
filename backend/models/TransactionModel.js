77295f3df5b6bb5b480253fa5fb9a487377d3fc6
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-11-15 16:10:47 -0300

    Update Transaction Model 😃
    Add: 👍
    - Required Values not present throw a default error 🚫
    
    - Validators: 🍫
        - Each data in the Schema has a
    way to validate 🤓
        - If the validator return false it'll throw a Error 🚫

--- backend/models/TransactionModel.js
+++ backend/models/TransactionModel.js
@@ -1,44 +1,80 @@
 import mongoose from 'mongoose'
 const { Schema, model } = mongoose
+import moment from 'moment'
 
 export const TransactionModel = model(
     'transaction',
     Schema({
         description: {
             type: String,
-            required: true,
+            required: [true, 'Description is required! (String)'],
+            minlength: [1, 'Minimum length of Description is one'],
         },
         value: {
             type: Number,
-            required: true,
+            required: [true, 'Value is required! (Number)'],
+            min: [0, 'Minimum value is zero'],
         },
         category: {
             type: String,
-            required: true,
+            required: [true, 'Category is required! (String)'],
+            minlength: [1, 'Minimum length of Category is one'],
         },
         year: {
             type: Number,
-            required: true,
+            required: [true, 'Year is required! (Number)'],
+            validate: {
+                validator: v => moment(v, 'YYYY', true).isValid(),
+                message: props =>
+                    `'${props.value}' is not a valid year or year format!`,
+            },
         },
         month: {
             type: Number,
-            required: true,
+            required: [true, 'Month is required! (Number)'],
+            validate: {
+                validator: v => moment(v, 'M', true).isValid(),
+                message: props =>
+                    `'${props.value}' is not a valid month or month format`,
+            },
         },
         day: {
             type: Number,
-            required: true,
+            required: [true, 'Day is required! (Number)'],
+            validate: {
+                validator: v => moment(v, 'D', true).isValid(),
+                message: props =>
+                    `'${props.value}' is not a valid day or day format`,
+            },
         },
         yearMonth: {
             type: String,
-            required: true,
+            required: [true, 'yearMonth is required! (String)'],
+            validate: {
+                validator: v => moment(v, 'YYYY-MM', true).isValid(),
+                message: props =>
+                    `'${props.value}' is not a valid yearMonth format (YYYY-MM)`,
+            },
         },
         yearMonthDay: {
             type: String,
-            required: true,
+            required: [true, 'yearMonthDay is required! (String)'],
+            validate: {
+                validator: v => moment(v, 'YYYY-MM-DD', true).isValid(),
+                message: props =>
+                    `'${props.value}' is not a valid yearMonthDay format (YYYY-MM-DD)`,
+            },
         },
         type: {
             type: String,
-            required: true,
+            required: [true, 'type is required! (String)'],
+            minlength: [1, 'Minimum length of type is one'],
+            maxlength: [1, 'Maximum length of type is one'],
+            validate: {
+                validator: v => /^\+$|^-$/.test(v),
+                message: props =>
+                    `'${props.value}' is not a valid type [Types: '+' || '-']`,
+            },
         },
         lastModified: {
             type: Date,
