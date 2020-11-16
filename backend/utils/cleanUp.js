60bc8009aedd3c9743ad1c523ef23bf90a61a7e7
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-11-16 13:43:55 -0300

    Update cleanUp (utils) 😃
    Now: 👍
    - When something happen instead in exit the process it:
        - Emits the "cleanup" event
        - In cleanup event it executes the callback and then exit process
        - When process is exited it prints "Bye bye" in console 🐱

--- backend/utils/cleanUp.js
+++ backend/utils/cleanUp.js
@@ -3,30 +3,43 @@ const { logger } = db
 
 /**
  * Default callback for cleanUp function
- * @return {void}
+ * @return {Promise<void>}
  */
-const defaultCallback = () => logger('info', 'Exited!')
+const defaultCallback = async () => {
+    return new Promise(resolve => {
+        resolve(logger('info', 'Exited!'))
+    })
+}
 
 /**
  * run the callback when the process is finished by any cause
  * @param callback The callback to make the cleanUp
  */
 const cleanUp = (callback = defaultCallback) => {
-    process.on('cleanup', callback)
+    process.on('cleanup', async () => {
+        await callback()
+        process.exit()
+    })
 
     process.on('exit', () => {
-        process.emit('cleanup')
+        logger('info', 'Bye bye~!')
     })
 
     process.on('SIGINT', () => {
         logger('warn', 'Ctrl-C...')
-        process.exit()
+        process.emit('cleanup')
     })
 
     process.on('uncaughtException', err => {
         logger('warn', 'Uncaught Exception...')
         logger('error', err.message)
-        process.exit(99)
+        process.emit('cleanup')
+    })
+
+    process.on('unhandledRejection', err => {
+        logger('warn', 'Unhandled Rejection...')
+        logger('error', err.message)
+        process.emit('cleanup')
     })
 }
 
